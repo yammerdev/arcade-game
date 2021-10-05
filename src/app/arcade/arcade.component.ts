46c6a5d7bc278aafe39b4eae53541216edece92c
Liam Obrochta <lao1711@g.rit.edu>
2021-10-04 21:25:55 -0400

    finish initial ui pass

--- src/app/arcade/arcade.component.ts
+++ src/app/arcade/arcade.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { GameComponent } from './game/game.component';
 
+interface game {
+  name: string
+  cost: number
+  description: string
+}
+
 @Component({
   selector: 'app-arcade',
   templateUrl: './arcade.component.html',
@@ -10,6 +16,27 @@ import { GameComponent } from './game/game.component';
 export class ArcadeComponent implements OnInit {
 
   constructor() { }
+
+
+  public GAME_LIST: game[] = [
+    { 
+      name: 'MINECRAFT', 
+      cost: 1.5, 
+      description: "test"
+    },
+    {
+      name: 'Pac-Man', 
+      cost: 1, 
+      description: "test"
+    },
+    {
+      name: 'League of Legends', 
+      cost: 4, 
+      description: "test"
+    }
+  ]
+
+ // public selectedGame:game = null
 
   ngOnInit(): void {
   }
--- src/app/arcade/game/game.component.ts
+++ src/app/arcade/game/game.component.ts
@@ -11,4 +11,8 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  playGame() {
+    console.log("hello")
+  }
 }
